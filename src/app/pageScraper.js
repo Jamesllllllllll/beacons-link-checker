7d6a4bd4fe6e0bdb3c03bfb5ee3a76e3583a01e7
James <jkeezer@pm.me>
2023-08-24 23:26:35 -0400

    Remove test scraper, replace with Beacons

--- src/app/pageScraper.js
+++ src/app/pageScraper.js
@@ -1,17 +1,17 @@
 const scraperObject = {
-	url: 'http://books.toscrape.com',
+	url: 'https://beacons.ai/duckytheyorkie',
 	async scraper(browser){
 		let page = await browser.newPage();
 		console.log(`Navigating to ${this.url}...`);
 		await page.goto(this.url);
 		// Wait for the required DOM to be rendered
-		await page.waitForSelector('.page_inner');
+		await page.waitForSelector('.Links');
 		// Get the link to all the required books
-		let urls = await page.$$eval('section ol > li', links => {
+		let urls = await page.$$eval('center.RowLink', links => {
 			// Make sure the book to be scraped is in stock
-			links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+			// links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
 			// Extract the links from the data
-			links = links.map(el => el.querySelector('h3 > a').href)
+			links = links.map(el => el.querySelector('a').href)
 			return links;
 		});
 		console.log(urls);
