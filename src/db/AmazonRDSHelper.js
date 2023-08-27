9444d07ccc1057af33743f068e49cb3dfdb5e404
mdwiltfong <wiltfong.michael1@gmail.com>
2023-08-26 23:47:02 -0400

    pingDB command with updated test

--- src/db/AmazonRDSHelper.js
+++ src/db/AmazonRDSHelper.js
@@ -1,6 +1,9 @@
 const { Client } = require("pg");
 const dotenv = require("dotenv");
-console.log(dotenv.config());
+const path = require("path");
+const { env } = require("process");
+const envFile = dotenv.config({ path: path.resolve(__dirname, "./.env") });
+console.log(envFile);
 class AmazonRDSHelper {
   #pgClient;
   #rdsEndPoint;
@@ -8,17 +11,30 @@ class AmazonRDSHelper {
   constructor() {
     this.#rdsEndPoint = process.env.RDS_ENDPOINT;
     this.#rdsPort = process.env.RDS_PORT;
-  }
-  async connectClient() {
     this.#pgClient = new Client({
       user: process.env.RDS_USERNAME,
       host: this.#rdsEndPoint,
       database: process.env.RDS_DATABASE,
       password: process.env.RDS_PASSWORD,
+      port: this.#rdsPort,
     });
   }
   getClient() {
     return this.#pgClient;
+  }
+  async pingDb() {
+    try {
+      this.#pgClient.connect();
+      const res = await this.#pgClient.query("SELECT 1");
+      console.log("<===========================>");
+      console.log(res);
+      console.log(res.rows[0]);
+      return res.rows[0];
+    } catch (error) {
+      console.log(error);
+    } finally {
+      await this.#pgClient.end();
+    }
   }
 }
 
