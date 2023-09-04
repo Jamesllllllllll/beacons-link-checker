16711848cc3629be9b91c2eb608fb739d10be2e9
mdwiltfong <wiltfong.michael1@gmail.com>
2023-09-04 15:50:00 -0400

    PuppeteerHandler class and test file

---
+++ src/app/puppeteer/tests/PuppeteerHandler.test.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+const { puppeteerHandler } = require("../PuppeteerHandler");
+//@Jamesllllllllll if you could help with these tests that would be great!
+describe("PuppeteerHandler", () => {
+  it("should be able to navigate to a page", async () => {});
+
+  it("should be able to extract links from page", async () => {});
+
+  it("should be able to close browser", async () => {});
+});
