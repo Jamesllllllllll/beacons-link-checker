bd24b5c3fd32734983d1eb92eeafbc7f838ee57c
James <jkeezer@pm.me>
2023-09-05 15:06:46 -0400

    Install Jest / Test for beacons route

--- src/app/puppeteer/tests/PuppeteerHandler.test.js
+++ src/app/puppeteer/tests/PuppeteerHandler.test.js
@@ -1,10 +1,24 @@
-const assert = require("assert");
-const { puppeteerHandler } = require("../PuppeteerHandler");
+// const assert = require('assert');
+const puppeteerHandler = require('../PuppeteerHandler');
+
 //@Jamesllllllllll if you could help with these tests that would be great!
-describe("PuppeteerHandler", () => {
-  it("should be able to navigate to a page", async () => {});
+describe('PuppeteerHandler', () => {
+  it('should be able to navigate to a page', async () => {
+    // Assemble
+    const actualPage = 'https://google.com';
+    const browser = puppeteerHandler;
+    console.log(browser);
 
-  it("should be able to extract links from page", async () => {});
+    // Act
+    // await puppeteerHandler.startBrowser();
+    // await puppeteerHandler.goToSite('https://google.com');
+    // const currentPage = puppeteerHandler.page.url();
 
-  it("should be able to close browser", async () => {});
+    // // Assert
+    // expect(currentPage).toEqual(actualPage);
+  });
+
+  it('should be able to extract links from page', async () => {});
+
+  it('should be able to close browser', async () => {});
 });
