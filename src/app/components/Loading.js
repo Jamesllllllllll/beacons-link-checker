c0131b41645337ce458636b00429f8964b5f3a84
James <jkeezer@pm.me>
2023-09-06 14:09:28 -0400

    Update route with .env BROWSER_PATH

--- src/app/components/Loading.js
+++ src/app/components/Loading.js
@@ -2,16 +2,16 @@ import styles from "./Loading.module.css";
 
 export default function Loading() {
   return (
-    <div class={styles.spinnerBox}>
-      <div class={`${styles.blueOrbit} ${styles.leo}`}></div>
+    <div className={styles.spinnerBox}>
+      <div className={`${styles.blueOrbit} ${styles.leo}`}></div>
 
-      <div class={`${styles.greenOrbit} ${styles.leo}`}></div>
+      <div className={`${styles.greenOrbit} ${styles.leo}`}></div>
 
-      <div class={`${styles.redOrbit} ${styles.leo}`}></div>
+      <div className={`${styles.redOrbit} ${styles.leo}`}></div>
 
-      <div class={`${styles.whiteOrbit} ${styles.w1} ${styles.leo}`}></div>
-      <div class={`${styles.whiteOrbit} ${styles.w2} ${styles.leo}`}></div>
-      <div class={`${styles.whiteOrbit} ${styles.w3} ${styles.leo}`}></div>
+      <div className={`${styles.whiteOrbit} ${styles.w1} ${styles.leo}`}></div>
+      <div className={`${styles.whiteOrbit} ${styles.w2} ${styles.leo}`}></div>
+      <div className={`${styles.whiteOrbit} ${styles.w3} ${styles.leo}`}></div>
     </div>
   );
 }
