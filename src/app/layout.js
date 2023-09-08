7967ac5a53bfb319e027071cdb6cae4b08ba7f11
James <jkeezer@pm.me>
2023-09-08 00:48:06 -0400

    AuthJS working with Google Provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Header from './components/Header';
+import { Provider } from './components/provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+          <Header />
+          {children}
+        </Provider>
+      </body>
     </html>
   );
 }
