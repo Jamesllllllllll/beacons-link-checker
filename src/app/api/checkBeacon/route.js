5d001f5d0693caca7dba009ec1e31e7ff861f116
James <jkeezer@pm.me>
2023-09-11 13:23:39 -0400

    Logging each page closed

--- src/app/api/checkBeacon/route.js
+++ src/app/api/checkBeacon/route.js
@@ -89,6 +89,7 @@ export async function GET(req, res) {
   console.log('Closing browser...');
   const pages = await browser.pages();
   for (let i = 0; i < pages.length; i++) {
+    console.log(`Closing page: ${pages[i]}`)
     await pages[i].close();
   }
   console.log('All pages closed.');
