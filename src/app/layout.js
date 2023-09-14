4561ae153f432f0cf5add9084331979f6602b489
James <jkeezer@pm.me>
2023-09-13 21:48:57 -0400

    Disable Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Header from './components/Header';
+// import Header from './components/Header';
 import { Provider } from './components/provider';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          <Header />
+          {/* <Header /> */}
           {children}
         </Provider>
       </body>
