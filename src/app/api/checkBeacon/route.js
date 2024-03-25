ccd615d24236e112341f444a525318564466b9e2
James <jkeezer@pm.me>
2024-03-25 01:15:14 -0400

    Add retry & correct error message

--- src/app/api/checkBeacon/route.js
+++ src/app/api/checkBeacon/route.js
@@ -58,17 +58,35 @@ export async function goToSite(browser, url) {
 }
 
 export async function fetchLinks(page) {
-  // const noAccount = await page.$x("//div[contains(text()='No Beacons account associated with')]");
-  // if (noAccount) {
-  //   console.log('No account with this username.')
-  //   console.log(noAccount)
-  // }
-  const links = await page.$$eval('.RowLink', (links) => {
+  await page.waitForSelector('#root');
+  let message;
+  let links = await page.$$eval('.RowLink', (links) => {
     links = links
       .map((el) => el.querySelector('a').href)
       .filter((link) => !link.startsWith('https://beacons'));
-    return links;
+    const linkObjects = links.map(function (link) {
+      return { url: link, delay: null };
+    });
+    let delay = 0;
+    for (let i = 0; i < linkObjects.length; i++) {
+      linkObjects[i].delay = delay;
+      delay += 200;
+    }
+    return linkObjects;
   });
+
+  const noAccountFound = await page.evaluate((searchText) => {
+    const allElements = Array.from(document.querySelectorAll('*'));
+    return allElements.some((element) =>
+      element.textContent.includes(searchText)
+    );
+  }, 'No Beacons account associated with');
+
+  if (noAccountFound) {
+    console.log('No Beacons account associated with this URL.');
+    links.push('No account associated with this username');
+  }
+
   console.log(links);
   return links;
 }
@@ -89,10 +107,9 @@ export async function GET(req, res) {
 
   const page = await goToSite(browser, url);
 
-  const links = await fetchLinks(page);
+  const { links, message } = await fetchLinks(page);
 
   console.log('Closing browser...');
-
   const pages = await browser.pages();
 
   for (let i = 0; i < pages.length; i++) {
@@ -103,6 +120,6 @@ export async function GET(req, res) {
   console.log('All pages closed.');
   browser.close();
   console.log('Browser closed.');
-  
+
   return new NextResponse(JSON.stringify({ data: links }), { status: 200 });
 }
---
+++ src/app/page.jsx
@@ -0,0 +1,219 @@
+'use client';
+import React, { useState, useContext, useRef, useEffect } from 'react';
+import FormLabel from '@mui/material/FormLabel';
+import InputBase from '@mui/material/InputBase';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Fade from '@mui/material/Fade';
+import SingleLink from './components/SingleLink';
+import { LinearProgress } from '@mui/material';
+import { TransitionGroup } from 'react-transition-group';
+import { AppContext } from './context/AppContext';
+import LoadingMessage from './components/LoadingMessage';
+
+export default function Home() {
+  const [username, setUsername] = useState('');
+  const { links, setLinks, welcome, setWelcome } = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoadingMessages, setShowLoadingMessages] = useState(true);
+  const [error, setError] = useState(null);
+  
+  const checkBeacons = async () => {
+    try {
+      const response = await fetch(`/api/checkBeacon?username=${username}`, {
+        method: 'GET',
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.links) {
+          setLinks(data.links);
+        }
+        if (data.message) {
+          setError(data.message)
+        }
+      } else {
+        setError(response.statusText);
+        return new Error(response.statusText);
+      }
+    } catch (err) {
+      setError(err)
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError(null);
+    setShowLoadingMessages(false);
+    setLinks([]);
+    setWelcome(false);
+    await checkBeacons();
+    if (error === 'Internal Server Error') {
+      console.log('Retrying...')
+      setError('Retrying...')
+      await checkBeacons();
+    }
+    setIsLoading(false);
+  };
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const [open, setOpen] = useState(true);
+  const inputRef = useRef();
+
+  const messages = [
+    'Thanks for trying our tool!',
+    'This will take 10-20 seconds...',
+    "A 'Headless' browser is loading up your profile",
+    'It will scan the page for your links, then we will check each one',
+    'If there are any broken links, you will see their status and can check for yourself!',
+    'Working...',
+  ];
+
+  return (
+    <>
+      <Paper
+        component="form"
+        sx={{
+          // p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        className="mx-4 sm:mx-0"
+        onSubmit={checkBeacons}
+      >
+        <FormLabel className="sr-only">Enter your Beacons username</FormLabel>
+        <Tooltip
+          title="Check your Beacons page for broken links"
+          arrow
+          enterDelay={500}
+          leaveDelay={200}
+          open={open}
+          onFocus={() => setOpen(false)}
+        >
+          <Stack
+            direction="row"
+            alignItems="center"
+            width="100%"
+            className="p-2"
+          >
+            <Typography className="font-bold ml-2">beacons.ai/</Typography>
+            <InputBase
+              className="pl-[2px] mt-[1px]"
+              placeholder="username"
+              required
+              size="medium"
+              inputRef={inputRef}
+              value={username}
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+            />
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              size="medium"
+              className="self-center"
+              disabled={isLoading}
+            >
+              Go
+            </Button>
+          </Stack>
+        </Tooltip>
+      </Paper>
+      <div className="flex flex-col items-center gap-y-4 w-11/12">
+        {welcome && (
+          <>
+            <Fade in={welcome} out={!welcome} timeout={1000}>
+              <Alert
+                severity="info"
+                sx={{ mx: 'auto', my: 4, backgroundColor: '#d5fffe' }}
+              >
+                <Typography sx={{ fontWeight: 700 }}>
+                  What is this for?
+                </Typography>
+                <Typography>
+                  Check for broken links on your <em>Link in Bio</em> page so
+                  your fans always have access to your content.
+                </Typography>
+              </Alert>
+            </Fade>
+            <Fade in={welcome} out={!welcome} timeout={2000}>
+              <Typography>
+                Don&apos;t have a Beacons Account? Try out the tool with one of
+                these usernames:
+              </Typography>
+            </Fade>
+            <Fade in={welcome} out={!welcome} timeout={3000}>
+              <Stack direction="row" gap={4} flexWrap="wrap">
+                <Button
+                  variant="outlined"
+                  onClick={() => setUsername('duckytheyorkie')}
+                >
+                  duckytheyorkie
+                </Button>
+                <Button variant="outlined" onClick={() => setUsername('james')}>
+                  james
+                </Button>
+              </Stack>
+            </Fade>
+          </>
+        )}
+
+        {isLoading && (
+          <Box sx={{ width: 300, alignSelf: 'center' }}>
+            <LinearProgress />
+            {showLoadingMessages && <LoadingMessage messages={messages} />}
+          </Box>
+        )}
+
+        {/* Show heading if links exist */}
+        {links.length > 0 &&
+          links[0] !== 'No links found' &&
+          links[0] !== 'No account associated with this username' && (
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              className="w-full"
+            >
+              <h2 className="text-xl font-semibold m-0">Your Beacons Links:</h2>
+              <Button
+                onClick={() => {
+                  setLinks([]);
+                  setUsername('');
+                  inputRef.current.focus();
+                }}
+                variant="outlined"
+                size="small"
+              >
+                Clear
+              </Button>
+            </Stack>
+          )}
+
+        {/* Show links */}
+        {error !== 'No account associated with this username' && (
+          <TransitionGroup>
+            {links.map((link) => (
+              <SingleLink
+                key={link.url}
+                url={link.url}
+                delay={link.delay}
+                style={{ transitionDelay: `${link.delay}ms` }}
+              />
+            ))}
+          </TransitionGroup>
+        )}
+
+        {error && <div>{error}</div>}
+      </div>
+    </>
+  );
+}
